2edb52cc913661d371d67f18a92de47e4b391ac0
Solovitska <146887721+Solovitska@users.noreply.github.com>
2024-02-21 10:57:41 +0200

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,45 +2,48 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const formElement = document.querySelector('.form');
-const inputDelayElement = formElement.elements.delay;
-const radioStateElement = formElement.elements.state;
+const form = document.querySelector('.form');
+const inputDelay = form.elements.delay;
+const radioState = form.elements.state;
 
-formElement.addEventListener('submit', handlerSubmit);
+form.addEventListener('submit', handlerSubmit);
 
 function handlerSubmit(event) {
   event.preventDefault();
-  delayHandler(inputDelayElement.value);
+  delayHandler(inputDelay.value);
   event.target.reset();
 }
 
 function delayHandler(delay) {
   if (delay > 0) {
-    const promise = radioStateElement.value === 'fulfilled'
-      ? Promise.resolve(`✅ Fulfilled promise in ${delay}ms`)
-      : Promise.reject(`❌ Rejected promise in ${delay}ms`);
+    const promise =
+      radioState.value === 'fulfilled'
+        ? Promise.resolve(`✅ Fulfilled promise in ${delay}ms`)
+        : Promise.reject(`❌ Rejected promise in ${delay}ms`);
 
     setTimeout(() => {
-      promise
-        .then(value => {
-          iziToast.show({
-            message: value,
-            backgroundColor: 'rgba(82, 223, 79, 0.3)',
-            position: 'topCenter',
-          });
-        })
-        .catch(value => {
-          iziToast.show({
-            message: value,
-            backgroundColor: 'rgba(223, 79, 79, 0.3)',
-            position: 'topCenter',
-          });
-        });
+      promise;
     }, delay);
+
+    promise
+      .then(value => {
+        iziToast.show({
+          message: value,
+          backgroundColor: 'rgba(82, 223, 79, 0.3)',
+          position: 'topCenter',
+        });
+      })
+      .catch(value => {
+        iziToast.show({
+          message: value,
+          backgroundColor: 'rgba(223, 79, 79, 0.3)',
+          position: 'topCenter',
+        });
+      });
   } else {
     iziToast.show({
       message: 'Value must be more than 0',
-      backgroundColor: 'lightgrey',
+      backgroundColor: 'light-grey',
       position: 'topCenter',
     });
   }
