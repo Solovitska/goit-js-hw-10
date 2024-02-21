6001fc241524b89e99b5e0ce57401d33769c1f3b
Solovitska <146887721+Solovitska@users.noreply.github.com>
2024-02-21 10:52:06 +0200

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,9 +1,10 @@
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
 const formElement = document.querySelector('.form');
-const inputDelayElement = form.elements.delay;
-const radioStateElement = form.elements.state;
+const inputDelayElement = formElement.elements.delay;
+const radioStateElement = formElement.elements.state;
 
 formElement.addEventListener('submit', handlerSubmit);
 
@@ -15,30 +16,27 @@ function handlerSubmit(event) {
 
 function delayHandler(delay) {
   if (delay > 0) {
-    const promise =
-    radioStateElement.value === 'fulfilled'
-        ? Promise.resolve(`✅ Fulfilled promise in ${delay}ms`)
-        : Promise.reject(`❌ Rejected promise in ${delay}ms`);
+    const promise = radioStateElement.value === 'fulfilled'
+      ? Promise.resolve(`✅ Fulfilled promise in ${delay}ms`)
+      : Promise.reject(`❌ Rejected promise in ${delay}ms`);
 
     setTimeout(() => {
-      promise;
+      promise
+        .then(value => {
+          iziToast.show({
+            message: value,
+            backgroundColor: 'rgba(82, 223, 79, 0.3)',
+            position: 'topCenter',
+          });
+        })
+        .catch(value => {
+          iziToast.show({
+            message: value,
+            backgroundColor: 'rgba(223, 79, 79, 0.3)',
+            position: 'topCenter',
+          });
+        });
     }, delay);
-
-    promise
-      .then(value => {
-        iziToast.show({
-          message: value,
-          backgroundColor: 'rgba(82, 223, 79, 0.3)',
-          position: 'topCenter',
-        });
-      })
-      .catch(value => {
-        iziToast.show({
-          message: value,
-          backgroundColor: 'rgba(223, 79, 79, 0.3)',
-          position: 'topCenter',
-        });
-      });
   } else {
     iziToast.show({
       message: 'Value must be more than 0',
@@ -47,4 +45,3 @@ function delayHandler(delay) {
     });
   }
 }
-
